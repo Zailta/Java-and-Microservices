2257954c8371a2c8daf8dfc08ed0e30eeb83b690
Zailta <mannanbhat60@gmail.com>
2024-02-17 22:35:16 +0530

    Bubble sort

--- src/JavaBasics/SortingAlgorithms.java
+++ src/JavaBasics/SortingAlgorithms.java
@@ -2,14 +2,23 @@ package JavaBasics;
 
 
 import java.io.IOException;
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 
 
 public class SortingAlgorithms {
 	
-	public int[] bubbleSort(int[] input, int size) {
-		return null;
+	public static int[] bubbleSort(int[] input, int size) {
+		for(int  i =0; i<=size-2;i++) {
+			for(int j = 0; j<=size-2-i;j++) {
+				if(input[j] >input[j+1]) {
+					int temp = input[j];
+					input[j] = input[j+1];
+					input[j+1] = temp;
+				}
+			}
+		}
+		return input;
 		
 	}
 	
@@ -35,8 +44,9 @@ public class SortingAlgorithms {
 	public static void main(String[] args) throws IOException {
 		int[] arr = {52,23,45,67,12,56};
 		int[] selectionSort = selectionSort(arr, arr.length);
-		
-		System.out.println(Arrays.toString(selectionSort));
+		int[] bubbleSort = bubbleSort(arr, arr.length);
+		System.out.println("Sort using Selection Sort ->"+Arrays.toString(selectionSort));
+		System.out.println("Sort using Selection Sort ->"+Arrays.toString(bubbleSort));
 	}
 
 }
