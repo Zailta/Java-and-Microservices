46490edc4550c55e83cf22d137fb2b625dbe841e
Zailta <mannanbhat60@gmail.com>
2024-03-17 16:53:28 +0530

    printTrianglInverse

--- src/JavaBasics/PatternCreation.java
+++ src/JavaBasics/PatternCreation.java
@@ -94,10 +94,29 @@ public static void printTrianglConsecutiveNumbers(int rows) {
 	
 }
 
+public static void printTrianglInverse(int rows) {
+	
+	
+	for(int i = 1; i<=rows;i++) {
+		
+	
+		for(int j = 1; j<=rows;j++) {
+			if(j<=rows-i)
+			System.out.print(" ");	
+			else 
+				System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+}
+
+
+
 
 
 	public static void main(String[] args) {
-		printTrianglAP(4);
+		printTrianglInverse(4);
 	}
 
 }
