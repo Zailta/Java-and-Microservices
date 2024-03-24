404d88509d95c532d565ad7faa3572d357ef6b3b
Zailta <mannanbhat60@gmail.com>
2024-03-24 17:20:14 +0530

    Pyramid

--- src/JavaBasics/PatternCreation.java
+++ src/JavaBasics/PatternCreation.java
@@ -156,12 +156,28 @@ public static void starBridgePattern(int rows) {
 	
 }
 
+public static void pyramid(int rows) {
+	
+	for(int i =1; i<rows;i++) {
+		for(int j = rows-i-1;j>0; j--) {
+			System.out.print(" ");
+		}
+		for(int j = 0;j<2*i-1; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	
+}
+
+
 
 
 
 
 	public static void main(String[] args) {
-		starBridgePattern(4);
+		pyramid(5);
 	}
 
 }
