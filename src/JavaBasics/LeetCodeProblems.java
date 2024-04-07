a1fe3921ab5a03aba64c74a736576b4a5f3fc2d2
Zailta <mannanbhat60@gmail.com>
2024-04-07 16:49:33 +0530

    sort Binary array

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -128,15 +128,34 @@ public class LeetCodeProblems {
 
 
     }
+    
+    static int[] sortArray(int [] arr) {
+    	int i = 0; 
+    	int j = arr.length-1;
+    	while(i<=j) {
+    		if(arr[i] ==0) i++;
+    		else if (arr[j] == 1) j--;
+    		
+    		else if(arr[i] ==1 && arr[j] ==0) {
+    			arr[i] = 0;
+    			arr[j] =1;
+    			i++;
+        		j--;
+    		}
+    		
+    	}
+    	
+    	return arr;
+    }
+
+	
+
 
 	public static void main(String[] args) {
-		int[] arr = { 2, 5, 5, 11 };
-		int target = 10;
-		int[] twoSum = twoSum(arr, target);
-		for (int s : twoSum) {
-			System.out.println(s);
-		}
-
+		int[] arr = { 0,1,0,1,1,1,0};
+		
+		int[] sortArray = sortArray(arr);
+		
 	}
 
 }
