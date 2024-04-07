e62f1a694c7dba8bb37177622b0c1b01f7c4fcad
Zailta <mannanbhat60@gmail.com>
2024-04-07 21:16:19 +0530

    Merge sorted arrays - Leetcode

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -188,6 +188,47 @@ public class LeetCodeProblems {
         }
       }
     }
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0, j=0, k=0;
+        int [] res = new int[m+n];
+
+       while (i<m && j<n){
+        if(nums1[i]<=nums2[j]){
+            res[k] = nums1[i];
+            i++;
+            
+        }
+        else {
+            res[k] = nums2[j];
+            j++;
+           
+        }
+        k++;
+        
+       }
+
+        if(m == i){
+            while (j<n){
+            res[k] = nums2[j];
+            j++;
+            k++;
+            }
+        }
+         if(n == j){
+            while (i<m){
+            res[k] = nums1[i];
+            i++;
+            k++;
+            }
+        }
+
+       
+       for(int l = 0; l<nums1.length;l++){
+        nums1[l] = res[l];
+       }
+        
+    }
 
 	
 
