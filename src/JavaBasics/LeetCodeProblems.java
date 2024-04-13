9a28ae3a6fedd0c9c72db77ef102305b55505251
Zailta <mannanbhat60@gmail.com>
2024-04-13 15:28:43 +0530

    sum 2D array

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -309,10 +309,20 @@ public class LeetCodeProblems {
     	System.out.println(max);
     }
 
+     static void sumElement2D(int arr[][]) {
+    	 int sum = 0;
+    	for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				sum+=arr[i][j];
+			}
+		}
+    	System.out.println(sum);
+    }
+
 
 	public static void main(String[] args) {
 		 int arr[][] = {{1,2,3},{5,6,7},{8,9,10}};
-		maxElement2D(arr);
+		 sumElement2D(arr);
 	}
 
 }
