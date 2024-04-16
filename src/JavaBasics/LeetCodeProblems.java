e25bdc3682c54094c68b8b9da31ba180ba7ca446
Zailta <mannanbhat60@gmail.com>
2024-04-17 00:39:44 +0530

    Multiplication Matrix

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -410,6 +410,24 @@ public class LeetCodeProblems {
          }
          return res;
      }
+     
+     public static int[][] multiplymatrix(int [][]arr1, int [][]arr2){
+    	 if(arr1[0].length !=arr2.length)
+    		return null;
+    	 
+    	 int[][] res = new int[arr1.length][arr2[0].length];
+    	 for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2[0].length; j++) {
+				for (int k = 0; k < arr2.length; k++) {
+					res[i][j] += arr1[i][k]*arr2[k][j];
+				}
+				
+			}
+		}
+    	 
+    	 return res;	 
+     }
+     
 
 	public static void main(String[] args) {
 		
