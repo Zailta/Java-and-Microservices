065e93e3e6435ce64665d23d8ab1988c64febdb6
Zailta <mannanbhat60@gmail.com>
2024-04-17 02:55:43 +0530

    Search in a matrix:2 - efficient

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -464,6 +464,17 @@ public class LeetCodeProblems {
          return sum;
      }
      
+     public boolean searchMatrix(int[][] arr, int target) {
+         int m = arr.length, n = arr[0].length ;
+         int i = 0, j = n-1;
+         while(i<m && j>=0){
+             if(arr[i][j] == target ) return true;
+             else if(arr[i][j] > target) j--;
+             else i++;
+         }
+        return false; 
+     }
+     
 
 	public static void main(String[] args) {
 		
