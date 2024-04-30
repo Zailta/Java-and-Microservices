7c82e1964d54aaec8e06148a61121060b695828f
Zailta <mannanbhat60@gmail.com>
2024-05-01 01:04:48 +0530

    peakIndexInMountainArray - Leetcode Medium

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -749,6 +749,17 @@ public class LeetCodeProblems {
 
           return ans;
      }
+     public int peakIndexInMountainArray(int[] arr) {
+         int low = 1, high  = arr.length-2;
+         int peak  = 0;
+         while(low<=high){
+             int mid = low+(high - low)/2;
+             if(arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid]) return mid;
+             else if(arr[mid-1] > arr[mid] && arr[mid+1] < arr[mid]) high = mid-1;
+             else if(arr[mid-1] < arr[mid] && arr[mid+1] > arr[mid]) low = mid+1;
+         }
+        return 0; 
+     }
      
 
 	public static void main(String[] args) {
