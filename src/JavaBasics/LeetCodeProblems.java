6728b9531286dc93e7aa443515210dacce8335cd
Zailta <mannanbhat60@gmail.com>
2024-05-02 00:42:13 +0530

    leetcode Medium optimised

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -824,6 +824,28 @@ public class LeetCodeProblems {
         return right;
         
     }
+    
+    public int searchinRoatateArrayOptimised(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while (low<=high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+           else if(nums[mid] <= nums[nums.length-1]){
+            if( target > nums[mid]  && target <= nums[high])
+                low = mid+1;
+            else 
+                high  = mid-1;
+           }
+           else{
+            if( target >= nums[low]  && target < nums[mid])
+                high  = mid-1;
+            else 
+                low = mid+1;
+           }
+           
+        }
+        return -1;
+}
      
 
 	public static void main(String[] args) {
