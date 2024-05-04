f54f6c725c1dbf7a37b940fc4d3eacdddab13259
Zailta <mannanbhat60@gmail.com>
2024-05-04 23:26:18 +0530

    targetIndices = leetcode Binary Search

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -903,6 +903,18 @@ public class LeetCodeProblems {
        }
         return count;
     }
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+       List<Integer> a = new ArrayList<>();
+       for(int i = 0;i<nums.length;i++){
+           if (nums[i] == target){
+               a.add(i);
+           }else if(nums[i] > target){
+               break;
+           }
+       }
+       return a;
+   }
 
 	public static void main(String[] args) {
 		
