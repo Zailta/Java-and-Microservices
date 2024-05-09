a2af4bd9361547aba1439b76de44157818cacf47
Zailta <mannanbhat60@gmail.com>
2024-05-09 19:08:55 +0530

    367. Valid Perfect Square - optimised

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -1096,6 +1096,17 @@ public class LeetCodeProblems {
 		}
 		return minC; 
     }
+    public boolean isPerfectSquare(int num) {
+        long low = 0, high  = num;
+        while (low <= high){
+            long mid = low + (high  - low )/2;
+
+            if(mid*mid == num) return true;
+            else if (mid*mid > num) high = mid-1;
+            else low = mid+1;
+        }
+        return false;
+    }
 	public static void main(String[] args) {
 
 		/*
