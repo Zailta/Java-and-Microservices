cfa81e749d9099cfed905070d7154813a01d769e
Zailta <mannanbhat60@gmail.com>
2024-05-14 14:08:48 +0530

    1342. Number of Steps to Reduce a Number to Zero

--- src/JavaBasics/LeetCodeProblems.java
+++ src/JavaBasics/LeetCodeProblems.java
@@ -1118,6 +1118,21 @@ public class LeetCodeProblems {
 
         return climbStairs(n-1) + climbStairs(n-2);
     }
+    public int numberOfSteps(int num) {
+        return helper(num, 0);     
+    }
+
+    public static int helper(int num, int steps){
+        if(num == 0) return steps;
+
+        if(num %2  == 0) return helper(num/2, steps+1);
+
+        return helper(num-1, steps+1) ;
+
+
+    }
+    
+    
 	public static void main(String[] args) {
 
 		/*
